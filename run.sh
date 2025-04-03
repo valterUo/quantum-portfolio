852af74010a3e4cdb6d127b9b06f726eb2567452
Uotila Valter <uotilav2@login4.triton.aalto.fi>
2025-04-03 20:51:06 +0300

    Add results

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=24:00:00
 #SBATCH --mem-per-cpu=5000MB
 #SBATCH --cpus-per-task=1
-#SBATCH --array=85-85
+#SBATCH --array=40-44
 
 echo "Running job array ${SLURM_ARRAY_TASK_ID} on $(hostname)"
 echo "Starting at $(date)"
